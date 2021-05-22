c7b663e74dc6f14e7f7d56127243e0d8b9136a94
TomeQ2k <48593674+TomeQ2k@users.noreply.github.com>
2021-05-22 23:57:54 +0200

    Update DbContextAppConfig.cs

--- MyBooking.Application/AppConfigs/DbContextAppConfig.cs
+++ MyBooking.Application/AppConfigs/DbContextAppConfig.cs
@@ -11,8 +11,8 @@ namespace MyBooking.Application.AppConfigs
         public static IServiceCollection ConfigureDbContext(this IServiceCollection services, IConfiguration configuration)
             => services.AddDbContext<DataContext>(options =>
             {
-                options.UseLazyLoadingProxies();
                 options.UseNpgsql(configuration.GetConnectionString(AppSettingsKeys.ConnectionString), b => b.MigrationsAssembly("MyBooking.Application"));
+                options.UseLazyLoadingProxies();
             });
     }
 }
